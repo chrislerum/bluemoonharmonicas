c791aa9bfdff36319a5f694124ed5b485e354049
Chris Lerum <chris.lerum@gmail.com>
2014-01-08 18:21:56 -0500

    deploy and seeds

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,8 @@ set :deploy_to, '/home/deployer/sites/bluemoonharmonicas'
 # set :pty, true
 
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+#set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
@@ -21,8 +22,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
