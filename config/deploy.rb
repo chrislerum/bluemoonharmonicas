dcf135a25c06298a494b35e5ce8f242197fa1f9e
Chris Lerum <chris.lerum@gmail.com>
2014-01-08 18:20:41 -0500

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,8 @@ set :deploy_to, '/home/deployer/sites/bluemoonharmonicas'
 # set :log_level, :debug
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
