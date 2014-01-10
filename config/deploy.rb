719e5aaff334f5a81e9b35d7091b75a17fa0c245
Chris Lerum <chris.lerum@gmail.com>
2014-01-10 11:02:41 -0500

    x

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,8 @@
 set :application, 'bluemoonharmonicas'
 set :repo_url, 'git@github.com:chrislerum/bluemoonharmonicas.git'
-
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
 set :deploy_to, '/home/deployer/sites/bluemoonharmonicas'
-# set :scm, :git
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
- #set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
 
 namespace :deploy do
 
