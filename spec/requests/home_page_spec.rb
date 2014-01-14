b8070b68b1657a94af6747ebf43f75b3ca42f196
Chris Lerum <chris.lerum@gmail.com>
2014-01-14 06:38:46 -0500

    show 'image missing' if no main harp image

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -10,3 +10,14 @@ describe "home page shows harp model, and a harp with a main image" do
     click_on 'MS Series'
   end
 end
+
+describe "home page shows harp model, and a harp without a main image(shows default image instead)" do
+  it "shows the item when click on that model" do
+    harp_model = create(:harp_model, name: 'MS Series')
+    item = create(:item, harp_model: harp_model)
+    visit root_path
+    click_on 'Products'
+    click_on 'MS Series'
+    expect(page.find('td a img')['src']).to have_content 'default_main_image.jpg'
+  end
+end
