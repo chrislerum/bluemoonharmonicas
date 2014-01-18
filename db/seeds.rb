b02753fe08ccc620d904111bce6b06ee803aaf0c
Chris Lerum <chris.lerum@gmail.com>
2014-01-18 17:18:57 -0500

    stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ puts "SEEDS!"
 ### Default users
 
 #User.create!(email: 'chris.lerum@gmail.com', name: 'Chris Lerum', password: 'urururyt', password_confirmation: 'urururyt').promote_to_admin
-AdminUser.create!(:email => 'thalchak@tampabay.rr.com', :password => 'bmh2588uf', :password_confirmation => 'bmh2588uf')
+AdminUser.create(:email => 'thalchak@tampabay.rr.com', :password => 'bmh2588uf', :password_confirmation => 'bmh2588uf')
 
 #Cart.create
 #Variant.update_all(quantity: 10)
