01e6a3242e81f822632a4cee45670617565f70e6
Chris Lerum <chris.lerum@gmail.com>
2014-10-26 19:44:21 -0400

    x

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -37,31 +37,31 @@ ActiveAdmin.register Order do
         else
           ap order
           #table_for order.cart.line_items.each do
-            #column "Type" do |line_item|
-              #line_item.purchasable.class
-            #end
-            #column "Name" do |line_item|
-              #line_item.purchasable.name
-            #end
-            #column "Quantity" do |line_item|
-              #line_item.quantity.to_s
-            #end
-            #column "Unit Price" do |line_item|
-              #humanized_money_with_symbol line_item.unit_price
-            #end
-            #column "Subtotal" do |line_item|
-              #humanized_money_with_symbol line_item.quantity * line_item.unit_price
-            #end
+          #column "Type" do |line_item|
+          #line_item.purchasable.class
           #end
+          #column "Name" do |line_item|
+          #line_item.purchasable.name
+          #end
+          #column "Quantity" do |line_item|
+          #line_item.quantity.to_s
+          #end
+          #column "Unit Price" do |line_item|
+          #humanized_money_with_symbol line_item.unit_price
+          #end
+          #column "Subtotal" do |line_item|
+          #humanized_money_with_symbol line_item.quantity * line_item.unit_price
+          #end
+          #end
+          panel "Total" do
+            total = 0
+            order.cart.line_items.each do |li|
+              total += li.quantity * li.unit_price
+            end
+            humanized_money_with_symbol total
+          end
         end
       end
-      #panel "Total" do
-        #total = 0
-        #order.cart.line_items.each do |li|
-          #total += li.quantity * li.unit_price
-        #end
-        #humanized_money_with_symbol total
-      #end
     end
   end
 
