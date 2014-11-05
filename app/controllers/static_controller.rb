20afe52effc5d337472efa29ebf49398b681f84f
Chris Lerum <chris.lerum@gmail.com>
2014-11-04 23:37:01 -0500

    Revert "bad idea, make home page editable from admin"
    This reverts commit 426936c37f5f49c6c90904ead571c3fcb0a04d2b.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
   def home
-    @home_page_html = CustomHtml.find_by_display_order(1)
+    @top_bit = CustomHtml.find_by_display_order(1)
+    @bottom_bit = CustomHtml.find_by_display_order(2)
   end
 
   def about
