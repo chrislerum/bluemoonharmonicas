e6004e81ad12fc65ecde1cf9c767cdfa9dc15271
Chris Lerum <chris.lerum@gmail.com>
2014-11-06 16:40:21 -0500

    x

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,20 +6,4 @@ class StaticController < ApplicationController
 
   def newsletter
   end
-  def list_users
-    sql = %{
-SELECT u.usename AS "User name",
-  u.usesysid AS "User ID",
-  CASE WHEN u.usesuper AND u.usecreatedb THEN CAST('superuser, create
-database' AS pg_catalog.text)
-       WHEN u.usesuper THEN CAST('superuser' AS pg_catalog.text)
-       WHEN u.usecreatedb THEN CAST('create database' AS
-pg_catalog.text)
-       ELSE CAST('' AS pg_catalog.text)
-  END AS "Attributes"
-FROM pg_catalog.pg_user u
-ORDER BY 1;
-    }
-    @user_list = ActiveRecord::Base.connection.execute(sql)
-  end
 end
